65e13937c8966bd580e049628f7a740e82a45137
Nagico <yjr888@vip.qq.com>
2023-09-30 13:09:05 +0800

    feat(python): 更新python接口

--- paperpilot-common-python/paperpilot_common/__init__.py
+++ paperpilot-common-python/paperpilot_common/__init__.py
@@ -1,1 +1,19 @@
-__version__ = "0.0.9"
+from paperpilot_common.utils.version import get_version
+
+VERSION = (0, 0, 9, "alpha", 0)
+
+__version__ = get_version(VERSION)
+
+
+def setup():
+    """
+    Configure the settings (this happens as a side effect of accessing the
+    first setting), configure logging and populate the app registry.
+    Set the thread-local urlresolvers script prefix if `set_prefix` is True.
+    """
+    from paperpilot_common.apps import apps
+    from paperpilot_common.conf import settings
+    from paperpilot_common.utils.log import configure_logging
+
+    configure_logging(fmt=settings.LOG_FMT, level=settings.LOG_LEVEL)
+    apps.populate(settings.INSTALLED_APPS)
